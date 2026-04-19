ea38a852abb6344db5592e41d5e8b7cec722b9e9
DominikYT <dominik.roblox.robux@gmail.com>
2026-04-19 22:01:19 +0200

    Update ConvertToExe.bat

--- ConvertToExe.bat
+++ ConvertToExe.bat
@@ -4,5 +4,6 @@ pip install pyinstaller
 cls
 set /p name=Enter Name of Discord Rat File (with .py): 
 python -m PyInstaller --onefile --noconsole --uac-admin %name%
+cls
 echo Converted to exe! Delete folder build. Check folder dist.
 pause
