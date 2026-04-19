94eeb2225ef6f0f6a48434cbade8de5a4f0e80e1
DominikYT <dominik.roblox.robux@gmail.com>
2026-04-19 16:28:15 +0200

    Create REQUIRED.bat

---
+++ REQUIRED.bat
@@ -0,0 +1,23 @@
+:: Required
+@echo off
+
+echo Installing Requirements in 5...
+timeout /t 1
+cls
+echo Installing Requirements in 4...
+timeout /t 1
+cls
+echo Installing Requirements in 3...
+timeout /t 1
+cls
+echo Installing Requirements in 2...
+timeout /t 1
+cls
+echo Installing Requirements in 1...
+timeout /t 1
+cls
+echo Installing Requirements in 0...
+timeout /t 1
+cls
+pip install requests pyinstaller
+pause
